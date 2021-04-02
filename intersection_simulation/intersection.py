1cb81c3db00e9e52ebcef9a06156947f829be2a3
Dominika Daraż <42532942+Precel2000@users.noreply.github.com>
2021-04-02 14:51:51 +0100

    Create main that initializes background

--- intersection_simulation/intersection.py
+++ intersection_simulation/intersection.py
@@ -173,3 +173,110 @@ class Vehicle(pygame.sprite.Sprite):
             + vehicles[self.direction][self.lane][self.index-1].image.get_rect().height
             + movingGap))):                
                 self.y -= self.speed
+
+# updates the timers of all signals
+def updateValues():
+    for i in range(0, noOfSignals):
+        if(i==currentGreen):
+            if(currentYellow==0):
+                signals[i].green-=1
+            else:
+                signals[i].yellow-=1
+        else:
+            signals[i].red-=1
+
+# this runs the simulation
+def repeat():
+    global currentGreen, currentYellow, nextGreen
+    while(signals[currentGreen].green>0):
+        # update values every second
+        updateValues()
+        time.sleep(1)
+    # change to yellow
+    currentYellow = 1   
+    for i in range(0,3):
+        for vehicle in vehicles[directionNumbers[currentGreen]][i]:
+            vehicle.stop=defaultStop[directionNumbers[currentGreen]]
+    while(signals[currentGreen].yellow>0):  
+        # update values every second
+        updateValues()
+        time.sleep(1)
+    # change to not yellow. Since the light is not yellow and not green it is red    
+    currentYellow = 0  
+    
+    signals[currentGreen].green = defaultGreen[currentGreen]
+    signals[currentGreen].yellow = defaultYellow
+    signals[currentGreen].red = defaultRed
+       
+    currentGreen = nextGreen 
+    nextGreen = (currentGreen+1)%noOfSignals
+    signals[nextGreen].red = signals[currentGreen].yellow+signals[currentGreen].green
+    repeat()
+
+
+
+# initialise the trafficSignal objects, clockise stating from top left 
+def initialize():
+    ts1 = TrafficSignal(0, defaultYellow, defaultGreen[0])
+    signals.append(ts1)
+    ts2 = TrafficSignal(ts1.yellow+ts1.green, defaultYellow, defaultGreen[1])
+    signals.append(ts2)
+    ts3 = TrafficSignal(defaultRed, defaultYellow, defaultGreen[2])
+    signals.append(ts3)
+    ts4 = TrafficSignal(defaultRed, defaultYellow, defaultGreen[3])
+    signals.append(ts4)
+    repeat()
+
+# method to initialise vehicles, type of vehicle, lane and direction are random
+def generateVehicles():
+    while(True):
+        vehicle_type = random.randint(0,3)
+        lane_number = random.randint(1,2)
+        temp = random.randint(0,99)
+        direction_number = 0
+        # distribution of type of vehicles generated
+        dist= [25,50,75,100]
+        if(temp<dist[0]):
+            direction_number = 0
+        elif(temp<dist[1]):
+            direction_number = 1
+        elif(temp<dist[2]):
+            direction_number = 2
+        elif(temp<dist[3]):
+            direction_number = 3
+        # construct the vehicles
+        Vehicle(lane_number, vehicleTypes[vehicle_type], direction_number, directionNumbers[direction_number])
+        time.sleep(1)
+
+class Main:
+    # create  thread to initialise traffic lights
+    thread1 = threading.Thread(name="initialization",target=initialize, args=())    
+    thread1.daemon = True
+    thread1.start() 
+    
+    # prepare background
+    black = (0, 0, 0)
+    white = (255, 255, 255) 
+    screenWidth = 1400
+    screenHeight = 800
+    screenSize = (screenWidth, screenHeight)
+    background = pygame.image.load('images/intersection.png')
+    screen = pygame.display.set_mode(screenSize)
+    pygame.display.set_caption("SIMULATION")
+
+    # load images of traffic signals
+    redSignal = pygame.image.load('images/signals/red.png')
+    yellowSignal = pygame.image.load('images/signals/yellow.png')
+    greenSignal = pygame.image.load('images/signals/green.png')
+    font = pygame.font.Font(None, 30)
+
+
+
+
+
+
+
+
+
+
+Main()
